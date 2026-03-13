7fbe7a18c55a0d94fd6ec6c789c094efbcc8d921
D32\zak <spseplzen@spseplzen.cz>
2026-03-13 12:23:51 +0100

    napisal

--- funkce/cvik11.py
+++ funkce/cvik11.py
@@ -20,13 +20,37 @@ import numpy as np
 #
 # print(druhe(lis))
 
-def prvo (cislo):
-    if cislo < 2:
-        return False
-    for i in range(2,cislo+1):
-        if cislo % i == 0:
-            return False
-    return True
+# def prvo (cislo):
+#     if cislo < 2:
+#         return False
+#     for i in range(2,cislo):
+#         if cislo % i == 0:
+#             return False
+#     return True
 
 
-print(prvo(13))
+# print(prvo(13))
+
+# def count_prvo (seznam):
+#     pocitadlo = 0
+#     for i in range(len(seznam)):
+#         if prvo(seznam[i]):
+#             pocitadlo+=1
+#     return pocitadlo
+#
+# print (count_prvo([1,5,13,25,16,7]))
+
+# def fce_add(a,b,c=10,d=10): #input overrides c & d defaults
+#     return a+b+c+d
+# print(fce_add(1,2))
+# print(fce_add(1,2,3)) #if each param has default, order don't matter
+
+def fukce_add(*args):
+    print(args)
+    soucet=0
+    for i in range(len(args)):
+        soucet +=args[i]
+    return soucet
+
+print(fukce_add(1,2,3,4))
+print(fukce_add(1,2))
